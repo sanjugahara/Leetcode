ec9e72bbc2d641b2a07b3da46b1d8bdf44c3a790
demon song <787423495@qq.com>
2016-12-23 08:10:18 +0800

    26.Remove Duplicates from Sorted Array

---
+++ src/com/daimens/algorithm/december/SolutionDay23_26.java
@@ -0,0 +1,32 @@
+package com.daimens.algorithm.december;
+
+
+/**
+ * 
+ * @author Demon Song
+ * 26.Remove Duplicates from Sorted Array
+ * Given a sorted array,remove the duplicates in place such each element appear only once and return the new length.
+ * Do not allocate extra space for another array, you must do this in place with constant memory.
+ * For example,
+ * Given input array nums = [1,1,2],
+ * Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively. 
+ * It doesn't matter what you leave beyond the new length.
+ *
+ */
+public class SolutionDay23_26 {
+	
+	//remove duplicates and return lenght
+	public int removeDuplicates(int[] nums){
+        if(nums.length <=1) return 1; 
+        
+		int j =0;
+		for(int i =1; i < nums.length; i++){
+			if(nums[i] !=nums[j]){
+				j++;
+				nums[j] = nums[i];
+			}
+		}
+		return j+1;
+	}
+
+}
