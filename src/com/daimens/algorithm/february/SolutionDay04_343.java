0970b4aafc414dc763bd0f53ed764adcc7d7bcb9
demon song <787423495@qq.com>
2017-02-04 14:18:03 +0800

    modify

--- src/com/daimens/algorithm/february/SolutionDay04_343.java
+++ src/com/daimens/algorithm/february/SolutionDay04_343.java
@@ -13,17 +13,20 @@ package com.daimens.algorithm.february;
 public class SolutionDay04_343 {
 	
 	public int integerBreak(int n) {
-        int[] dp = new int[59];
-		for(int i = 0; i < 59 ;i++){
+		int[] dp = new int[n+1];
+		for(int i = 0; i < n+1 ;i++){
 			//偶数
 			if(i%2 == 0){
 				int x = i / 2;
-				int m1 = Math.max((x-1) * (x + 1), x-1 <= 3 ? x-1 : dp[x-1] * (x + 1) <= 3 ? (x + 1) : dp[x + 1]);
-				int m2 = Math.max(x * x, x <= 3 ? x : dp[x] * x <= 3 ? x : dp[x]);
+				int m1 = Math.max((x - 1) * (x + 1),
+						(x - 1 <= 3 ? x - 1 : dp[x - 1]) * ((x + 1) <= 3 ? (x + 1) : dp[x + 1]));
+				int m2 = Math.max(x * x, (x <= 3 ? x : dp[x]) * (x <= 3 ? x : dp[x]));
 				dp[i] = Math.max(m1, m2);
 			}else{//奇数的情况
 				int x = i / 2;
-				dp[i] = Math.max(x * (x + 1), x <= 3 ? x : dp[x] * (x + 1) <= 3 ? (x + 1) : dp[x + 1]);
+				int m1 = Math.max(x * (x + 1), (x <= 3 ? x : dp[x]) *( (x + 1) <= 3 ? (x + 1) : dp[x + 1]));
+				int m2 = Math.max((x-1) * (x + 2), (x-1 <= 3 ? x-1 : dp[x-1]) *( (x + 2) <= 3 ? (x + 2) : dp[x + 2]));
+				dp[i] = Math.max(m1,m2);
 			}
 		}
         return dp[n];
