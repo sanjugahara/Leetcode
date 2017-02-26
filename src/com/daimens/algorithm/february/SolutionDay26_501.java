9ccf9625a5b3741b38a8b162cfc25d0ac7f795b5
demon song <787423495@qq.com>
2017-02-26 14:22:14 +0800

    523.Continuous Subarray Sum

--- src/com/daimens/algorithm/february/SolutionDay26_501.java
+++ src/com/daimens/algorithm/february/SolutionDay26_501.java
@@ -1,7 +1,7 @@
 package com.daimens.algorithm.february;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SolutionDay26_501 {
 	//传统解法
@@ -61,9 +61,36 @@ public class SolutionDay26_501 {
 //		return res;
 //	}
 	
+	//smart solution
 	public boolean checkSubarraySum(int[] nums, int k) {
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>() {
+			{
+				put(0, -1); //初始化操作
+			}
+		};;
+		
+		int runningSum = 0;
+		for (int i = 0; i < nums.length; i++) {
+			runningSum += nums[i];
+			
+			if (k != 0)
+				runningSum %= k;
+			
+			Integer prev = map.get(runningSum);
+			if (prev != null) {
+				if (i - prev > 1)
+					return true;
+			} else {
+				map.put(runningSum, i);
+			}
+		}
+		
 		return false;
 	}
+	
+	//iterator solution
+	
+	
 	public static void main(String[] args) {
 		SolutionDay26_501 day = new SolutionDay26_501();
 		int[] nums ={23,2,4,6,7};
