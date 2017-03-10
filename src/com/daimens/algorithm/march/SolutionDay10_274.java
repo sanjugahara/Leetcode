8662017e86a28535cea817c8be9a723fc2b595c4
demon song <787423495@qq.com>
2017-03-10 14:36:28 +0800

    274.H-index

---
+++ src/com/daimens/algorithm/march/SolutionDay10_274.java
@@ -0,0 +1,63 @@
+package com.daimens.algorithm.march;
+
+import java.util.Arrays;
+
+/**
+ * 
+ * @author DemonSong
+ * 274.H-index
+ * Given an array of citations (each citation is a non-negative integer) of a researcher,write a function to 
+ * compute the researcher's h-index.
+ * According to the definition of h-index of on Wikipedia:"A scientist has index h if h of his/her N papers have at
+ * least h citations each,and the other N-h papers have no more than h citations each."
+ * For example, given citations = [3,0,6,1,5],which means the researcher has 5 papers in total and each of them had
+ * received 3,0,6,1,5 citations respectively.Since the researcher has 3 papers with at least 3 citations each and 
+ * the remaining two with no more than 3 citations each,his h-index is 3.
+ * Note: If there are several possible values for h, the maximum one is taken as the h-index.
+ * Hint:
+ * 1. An easy approach is to sort the array first.
+ * 2. What are the possible values of h-index?
+ * 3. A faster approach is to use extra space.
+ *
+ */
+public class SolutionDay10_274 {
+	
+//	public int hIndex(int[] citations) {
+//	
+//		if(citations.length == 0) return 0;
+//		Arrays.sort(citations);
+//		
+//		int hIndex = 0;
+//		for (int i= citations.length-1; i >= 0; i--){
+//			if (citations[i] > hIndex){
+//				hIndex ++;
+//			}
+//		}
+//		
+//		return hIndex;
+//    }
+	
+	public int hIndex(int[] citations) {
+		int length = citations.length;
+		if(length == 0) return 0;
+		
+		int[] array2 = new int[length+1];
+		for (int i = 0; i < length; i++){
+			if(citations[i] > length){
+				array2[length] += 1;
+			}else{
+				array2[citations[i]] += 1;
+			}
+		}
+		
+		int t = 0;
+		for (int i = length; i >=0; i--){
+			t = t + array2[i];
+			if(t >= i){
+				return i;
+			}
+		}
+		
+		return 0;
+	}
+}
---
+++ src/com/daimens/algorithm/march/SolutionDay10_275.java
@@ -0,0 +1,19 @@
+package com.daimens.algorithm.march;
+
+/**
+ * 
+ * @author DemonSong
+ * 275.H-Index II
+ * Follow up for H-index: What if the citations array is sorted in ascending order? Could you optimize your algorithm?
+ * Hint:
+ * 1. Expected runtime complexity is in O(log n) and the input is sorted.
+ *
+ */
+public class SolutionDay10_275 {
+	
+	public int hIndex(int[] citations){
+		
+		return 0;
+	}
+
+}
