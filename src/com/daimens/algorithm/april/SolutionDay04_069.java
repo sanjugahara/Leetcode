024e4d5de7d2c3069fe55ea6f08cae0427fce360
demon song <787423495@qq.com>
2017-04-04 08:45:06 +0800

    sqrt logn solution

--- src/com/daimens/algorithm/april/SolutionDay04_069.java
+++ src/com/daimens/algorithm/april/SolutionDay04_069.java
@@ -10,22 +10,42 @@ package com.daimens.algorithm.april;
  */
 public class SolutionDay04_069 {
 	
+//	public int mySqrt(int x) {
+//		
+//		if(x <= 0) return 0;
+//		
+//		int i = 0, max = 0;
+//		for (i = (int) Math.pow(10, Math.log10(x) / 2); i * i <= x; i++) {
+//			if (i * i >= max) {
+//				max = i * i;
+//			} else {
+//				return i - 1;
+//			}
+//		}
+//		
+//		return i-1;
+//    }
+	
 	public int mySqrt(int x) {
+		if (x <= 0) return 0;
 		
-		if(x <= 0) return 0;
-		
-		int i = 0, max = 0;
-		for (;i * i <= x;i++){
-			if (i * i >= max){
-				max = i * i;
-			}
-			else{
-				return i-1;
+		int left = 1, right = x;
+
+		int ans = 0;
+		while (left <= right) {
+			int mid = left + (right - left) / 2;
+
+			if (mid > x / mid) { //避免溢出
+				right = mid - 1;
+			} else {
+
+				left = mid + 1;
+				ans = mid;
 			}
 		}
-		
-		return i-1;
-    }
+
+		return ans;
+	}
 	
 	public static void main(String[] args) {
 		System.out.println(Integer.MIN_VALUE);
