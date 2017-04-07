5d5c1d3441a4477183f5fca5ee18aa9106478bcc
demon song <787423495@qq.com>
2017-04-07 18:30:52 +0800

    50.pow(x,n)

---
+++ src/com/daimens/algorithm/april/SolutionDay07_050.java
@@ -0,0 +1,27 @@
+package com.daimens.algorithm.april;
+
+/**
+ * 
+ * @author DemonSong
+ * 50.Pow(x,n)
+ * Implement pow(x,n)
+ *
+ */
+public class SolutionDay07_050 {
+	
+	public double myPow(double x, int m) {
+		double temp = x;
+		if (m == 0)
+			return 1;
+		temp = myPow(x, m / 2);
+		if (m % 2 == 0)
+			return temp * temp;
+		else {
+			if (m > 0)
+				return x * temp * temp;
+			else
+				return (temp * temp) / x;
+		}
+	}
+
+}
