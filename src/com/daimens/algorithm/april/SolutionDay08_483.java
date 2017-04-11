51024bde0b2a7efc8ff17c9ac2d403b8f2c56ea3
demon song <787423495@qq.com>
2017-04-11 09:19:30 +0800

    483.Smallest Good Base

--- src/com/daimens/algorithm/april/SolutionDay08_483.java
+++ src/com/daimens/algorithm/april/SolutionDay08_483.java
@@ -2,6 +2,12 @@ package com.daimens.algorithm.april;
 
 import java.util.Arrays;
 
+import javax.swing.event.AncestorEvent;
+
+import com.sun.org.apache.bcel.internal.generic.StackConsumer;
+
+import sun.security.krb5.Asn1Exception;
+
 /**
  * 
  * @author DemonSong
@@ -31,15 +37,73 @@ import java.util.Arrays;
 public class SolutionDay08_483 {
 	public String smallestGoodBase(String n) {
 		
-		for (int j = 3; j <= Integer.parseInt(n);j++){
-			if (Integer.toString(Integer.parseInt(n), j).matches("1+")){
-				return Integer.toString(j);
-			}
+		long num = Long.parseLong(n);
+		
+		for (long i = 64; i >= 2; i--){
+			long r = (long)(Math.pow(num, 1.0 / i) + 1);
+			long index = binarySearch(2, r, i, num);
+			if (index != -1)
+				return String.valueOf(index);
 		}
-		return null;
+		
+		return String.valueOf(num-1);
+		
 	}
 	
+	private long binarySearch(long start, long end, long len, long num){
+		
+		while (start < end){
+			long mid = start + (end - start) / 2;
+			
+			//用math.pow 一点都不靠谱
+			long ans = 0, cur = 1;
+            for (int i = 0; i <= len; i++) {
+                ans += cur;
+                cur *= mid;
+            }
+			if (ans == num) return mid;
+            else if (ans < num){
+				start = mid + 1;
+			}else{
+				end = mid;
+			}
+		}
+		
+		return -1;
+	}
+	
+//	public String smallestGoodBase(String n) {
+//        long num = 0;
+//        for (char c : n.toCharArray()) num = num * 10 + c - '0';
+//        
+//        long x = 1;
+//        for (int p = 64; p >= 1; p--) {
+//            if ((x << p) < num) {
+//                long k = helper(num, p);
+//                if (k != -1) return String.valueOf(k);
+//            }
+//        }
+//        return String.valueOf(num - 1);
+//    }
+//    
+//    private long helper(long num, int p) {
+//        long l = 1, r = (long)(Math.pow(num, 1.0/p) + 1);
+//        while (l < r) {
+//            long mid = l + (r - l) / 2;
+//            long sum = 0, cur = 1;
+//            for (int i = 0; i <= p; i++) {
+//                sum += cur;
+//                cur *= mid;
+//            }
+//            if (sum == num) return mid;
+//            else if (sum > num) r = mid;
+//            else l = mid + 1;
+//        }
+//        return -1;
+//    }
+	
 	public static void main(String[] args) {
-			System.out.println(Integer.toString(767, 766));
+		SolutionDay08_483 day = new SolutionDay08_483();
+		day.smallestGoodBase("3541");
 	}
 }
