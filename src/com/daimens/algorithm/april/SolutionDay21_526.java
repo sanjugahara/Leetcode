cb59076d151cdcb9c4cf898bb6b7ce86b7298c3e
demon song <787423495@qq.com>
2017-04-21 19:35:59 +0800

    535.Encode and Decode TinyURL

---
+++ src/com/daimens/algorithm/april/SolutionDay21_526.java
@@ -0,0 +1,20 @@
+package com.daimens.algorithm.april;
+
+/**
+ * 
+ * @author DemonSong 
+ * 526.Beautiful Arrangement
+ * 
+ *         Suppose you have N integers from 1 to N. We define a beautiful
+ *         arrangement as an array that is constructed by these N numbers
+ *         successfully if one of the following is true for the ith position (1
+ *         ≤ i ≤ N) in this array:
+ * 
+ *         The number at the ith position is divisible by i. i is divisible by
+ *         the number at the ith position. Now given N, how many beautiful
+ *         arrangements can you construct?
+ *
+ */
+public class SolutionDay21_526 {
+
+}
