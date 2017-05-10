93475d7225c78041371f62157366be8c47e42cff
demon song <787423495@qq.com>
2017-05-10 23:59:56 +0800

    32.Longest Valid Parentheses

--- src/com/daimens/algorithm/may/SolutionDay02_032.java
+++ src/com/daimens/algorithm/may/SolutionDay02_032.java
@@ -50,34 +50,91 @@ public class SolutionDay02_032 {
 //		return max;
 //	}
 	
+//	public int longestValidParentheses(String s) {
+//		
+//		if (s.length() <= 1) return 0;
+//		char[] c = s.toCharArray();
+//		int curMax = 0;
+//		int[] longest = new int[s.length()];
+//		
+//		for (int i = 1; i < s.length(); i++){
+//			if (c[i] == ')'){
+//				if (c[i-1] == '('){
+//					longest[i] = (i - 2) >= 0 ? (longest[i - 2] + 2) : 2;
+//					curMax = Math.max(curMax, longest[i]);
+//				}
+//				else{
+//					if (i-longest[i-1] - 1 >= 0 && c[i-longest[i-1]-1] == '('){
+//						longest[i] = longest[i - 1] + 2
+//								+ ((i - longest[i - 1] - 2 >= 0) ? longest[i - longest[i - 1] - 2] : 0);
+//                        curMax = Math.max(longest[i],curMax);
+//					}
+//				}
+//			}
+//		}
+//		
+//		return curMax;
+//	}
+	
+//	public int longestValidParentheses(String s) {
+//		int n = s.length();
+//		Stack<Integer> stack = new Stack<>();
+//		char[] c = s.toCharArray();
+//		for (int i = 0; i < n; i++){
+//			if (c[i] == '('){
+//				stack.push(i);
+//			}else{
+//				if (!stack.isEmpty()){
+//					if (c[stack.peek()] == '(') stack.pop();
+//					else stack.push(i);
+//				}else{
+//					stack.push(i); //推入非法的下标
+//				}
+//			}
+//		}
+//		
+//		int max = 0;
+//		if (stack.isEmpty()) max = n; 
+//		else{
+//			int curr = n;
+//			int next = 0;
+//			while (!stack.isEmpty()){
+//				next = stack.pop();
+//				max = Math.max(max, curr-next-1);
+//				curr = next;
+//			}
+//			max = Math.max(max, curr);
+//		}
+//		return max;
+//	}
+	
 	public int longestValidParentheses(String s) {
+		int n = s.length();
+		int[] dp = new int[n];
 		
-		if (s.length() <= 1) return 0;
 		char[] c = s.toCharArray();
-		int curMax = 0;
-		int[] longest = new int[s.length()];
-		
-		for (int i = 1; i < s.length(); i++){
+		int max = 0;
+		for (int i = 0; i < n; i++){
 			if (c[i] == ')'){
-				if (c[i-1] == '('){
-					longest[i] = (i - 2) >= 0 ? (longest[i - 2] + 2) : 2;
-					curMax = Math.max(curMax, longest[i]);
+				if (i != 0 && c[i-1] == '('){
+					dp[i] = ((i - 2) != -1 ? dp[i-2] : 0) + 2;
 				}
-				else{
-					if (i-longest[i-1] - 1 >= 0 && c[i-longest[i-1]-1] == '('){
-						longest[i] = longest[i - 1] + 2
-								+ ((i - longest[i - 1] - 2 >= 0) ? longest[i - longest[i - 1] - 2] : 0);
-                        curMax = Math.max(longest[i],curMax);
+				
+				if (i != 0 && c[i-1] == ')'){
+					if (i - dp[i-1] -1 != -1 && c[i-dp[i-1]-1] == '('){
+						dp[i] = dp[i-1]+2 + (i -dp[i-1]-2 != -1 ? dp[i-dp[i-1]-2] : 0);
 					}
 				}
+				
+				max = Math.max(max, dp[i]);
 			}
 		}
 		
-		return curMax;
+		return max;
 	}
 
 	public static void main(String[] args) {
 		SolutionDay02_032 day = new SolutionDay02_032();
-		day.longestValidParentheses("()()");
+		day.longestValidParentheses("(()())");
 	}
 }
