56e6b553cd827dfd4cb20ede76c1509041be7559
demon song <787423495@qq.com>
2017-05-18 19:12:35 +0800

    3669.

--- src/com/daimens/algorithm/may/SolutionDay18_P3669.java
+++ src/com/daimens/algorithm/may/SolutionDay18_P3669.java
@@ -1,5 +1,8 @@
 package com.daimens.algorithm.may;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 /**
@@ -52,7 +55,68 @@ public class SolutionDay18_P3669 {
 	
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
+		int M = in.nextInt();
+		int[][] map = new int[M][2];
+		int[] T = new int[M];
+		for (int i = 0; i < M; i++){
+			map[i][0] = in.nextInt();
+			map[i][1] = in.nextInt();
+			T[i] = in.nextInt();
+		}
+		System.out.println(solve(map, T));
 		in.close();
 	}
 	
+	static int[][] dir = {{-1,0},{1,0},{0,-1},{0,1},{0,0}};
+	static final int INF = 1 << 30;
+	
+	private static int solve(int[][] map, int[] T){
+		int[][] distance = new int[512][512];
+		int[][] hit = new int[512][512];
+		
+		for (int i = 0; i < distance.length; i++){
+			Arrays.fill(distance[i], INF);
+			Arrays.fill(hit[i], INF);
+		}
+		distance[0][0] = 0;
+		int M = map.length;
+		int last = T[0];
+		for (int i = 0; i < M; i++){
+			int x = map[i][0];
+			int y = map[i][1];
+			last = Math.max(last, T[i]);
+			for (int[] d : dir){
+				int nx = x + d[0];
+				int ny = y + d[1];
+				if (nx >= 0 && nx < 512 && ny >= 0 && ny < 512){
+					hit[nx][ny] = Math.min(hit[nx][ny], T[i]);
+				}
+			}
+		}
+		
+		Queue<int[]> queue = new LinkedList<>();
+		queue.offer(new int[]{0,0});
+		if (hit[0][0] == 0) return -1; 
+		
+		int step = 0;
+		while (!queue.isEmpty()){
+			int size = queue.size();
+			step++;
+			for (int i = 0; i < size; i++){
+				int[] cur = queue.poll();
+				for (int k = 0; k < 4; k++){
+					int nx = cur[0] + dir[k][0];
+					int ny = cur[1] + dir[k][1];
+					if (nx >= 0 && nx < 512 && ny >= 0 && ny < 512 && step < hit[nx][ny] && distance[nx][ny] == INF){
+						distance[nx][ny] = step;
+						if(hit[nx][ny] > last){
+							return step;
+						}
+						queue.offer(new int[]{nx,ny});
+					}
+				}
+			}
+		}
+		return -1;
+	}
 }
