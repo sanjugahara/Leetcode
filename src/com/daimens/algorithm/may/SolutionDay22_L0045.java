31e5175d7445f4e1f50772c904cc84cdf9052652
demon song <787423495@qq.com>
2017-05-22 12:19:34 +0800

    45.jump game II

--- src/com/daimens/algorithm/may/SolutionDay22_L0045.java
+++ src/com/daimens/algorithm/may/SolutionDay22_L0045.java
@@ -24,4 +24,64 @@ package com.daimens.algorithm.may;
  */
 public class SolutionDay22_L0045 {
 
+//	public int jump(int[] nums) {
+//		if (nums.length == 1)
+//			return 0;
+//		int step = 1;
+//		int range = 0 + nums[0];
+//		int now = 0;
+//		while (range < nums.length - 1) {
+//			step++;
+//			int maxRange = 0;
+//			int index = -1;
+//			for (int i = now + 1; i <= range; i++) {
+//				if (i + nums[i] >= maxRange) {
+//					maxRange = i + nums[i];
+//					index = i;
+//				}
+//			}
+//			range = maxRange;
+//			now = index;
+//		}
+//		return step;
+//	}
+	
+//	public int jump(int[] nums) {
+//		int step = 0, range = 0, now = -1;
+//		while (range < nums.length - 1) {
+//			step++;
+//			int maxRange = 0;
+//			int index = -1;
+//			for (int i = now + 1; i <= range; i++) {
+//				if (i + nums[i] >= maxRange) {
+//					maxRange = i + nums[i];
+//					index = i;
+//				}
+//			}
+//			range = maxRange;
+//			now = index;
+//		}
+//		return step;
+//	}
+	
+	public int jump(int[] nums) {
+		int step = 0, edge = 0, maxRange = 0;
+		int len = nums.length;
+		for (int i = 0; i < len; i++){
+			if (i > edge){
+				step ++;
+				edge = maxRange;
+				maxRange = 0;
+			}
+			maxRange = Math.max(maxRange, i + nums[i]);
+		}
+		return step;
+	}
+
+	public static void main(String[] args) {
+		SolutionDay22_L0045 day = new SolutionDay22_L0045();
+		int[] nums = { 2, 1 };
+		day.jump(nums);
+	}
+
 }
