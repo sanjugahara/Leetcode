f7ba29ee7cf192bddf0c6dde2d76f96ad126d266
demon song <787423495@qq.com>
2017-05-27 14:18:09 +0800

    227.Basic Calculator II

--- src/com/daimens/algorithm/may/SolutionDay27_L0227.java
+++ src/com/daimens/algorithm/may/SolutionDay27_L0227.java
@@ -1,7 +1,6 @@
 package com.daimens.algorithm.may;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.Stack;
 
 /**
  * 
@@ -23,72 +22,105 @@ import java.util.Deque;
  */
 public class SolutionDay27_L0227 {
 	
+//	public int calculate(String s) {
+//		String ss = s.replace("+","#+").replace("-", "#-").replace("/", "#/").replace("*", "#*");
+//		String[] values = ss.split("#");
+//		int[] nums = new int[values.length];
+//		char[] operators = new char[values.length-1];
+//		
+//		int cnt1 = 0;
+//		int cnt2 = 0;
+//		for (int i = 0; i < values.length; i++){
+//			if (values[i].toCharArray()[0] == '+' ||
+//					values[i].toCharArray()[0] == '-' ||
+//						values[i].toCharArray()[0] == '*' ||
+//							 values[i].toCharArray()[0] == '/'){
+//				operators[cnt2++] = values[i].toCharArray()[0];
+//				nums[cnt1++] = Integer.parseInt(values[i].substring(1).trim());
+//			}else{
+//				nums[cnt1++] = Integer.parseInt(values[i].trim());
+//			}
+//		}
+//		
+//		Deque<Integer> numStack = new ArrayDeque<>();
+//		Deque<Character> operatorStack = new ArrayDeque<>();
+//		numStack.push(nums[0]);
+//		for (int i = 0; i < operators.length; i++){
+//			if (operators[i] == '*' || operators[i] == '/'){
+//				numStack.push(nums[i+1]);
+//				int a1 = numStack.pollFirst();
+//				int a2 = numStack.pollFirst();
+//				switch (operators[i]) {
+//				case '*':{
+//					numStack.push(a2 * a1);
+//				}
+//					break;
+//				case '/':{
+//					numStack.push(a2 / a1);
+//				}
+//					break;
+//				default:
+//					break;
+//				}
+//			}else{
+//				operatorStack.push(operators[i]);
+//				numStack.push(nums[i+1]);
+//			}
+//		}
+//		
+//		while (!operatorStack.isEmpty()){
+//			char opera = operatorStack.pollLast();
+//			int a1 = numStack.pollLast();
+//			int a2 = numStack.pollLast();
+//			switch (opera) {
+//			case '+':{
+//				numStack.addLast(a1 + a2);;
+//			}
+//				break;
+//			case '-':{
+//				numStack.addLast(a1 - a2);
+//			}
+//				break;
+//			default:
+//				break;
+//			}
+//		}
+//		
+//		return numStack.peek();
+//    }
+	
 	public int calculate(String s) {
-		String ss = s.replace("+","#+").replace("-", "#-").replace("/", "#/").replace("*", "#*");
-		String[] values = ss.split("#");
-		int[] nums = new int[values.length];
-		char[] operators = new char[values.length-1];
-		
-		int cnt1 = 0;
-		int cnt2 = 0;
-		for (int i = 0; i < values.length; i++){
-			if (values[i].toCharArray()[0] == '+' ||
-					values[i].toCharArray()[0] == '-' ||
-						values[i].toCharArray()[0] == '*' ||
-							 values[i].toCharArray()[0] == '/'){
-				operators[cnt2++] = values[i].toCharArray()[0];
-				nums[cnt1++] = Integer.parseInt(values[i].substring(1).trim());
-			}else{
-				nums[cnt1++] = Integer.parseInt(values[i].trim());
+		if (s == null || s.length() == 0) return 0;
+		char[] cs = s.toCharArray();
+		Stack<Integer> stack = new Stack<>();
+		char sign = '+'; //开始的数字一定为正
+		for (int i = 0, num = 0; i < cs.length; i++){
+			if (Character.isDigit(cs[i])){
+				num = num * 10 + cs[i] - '0';
+			}
+			if (i == cs.length -1 || !Character.isDigit(cs[i]) && cs[i] != ' '){ //操作符的情况
+				if (sign == '+'){
+					stack.push(num);
+				}
+				if (sign == '-'){
+					stack.push(-num);
+				}
+				if (sign == '*'){
+					stack.push(stack.pop() * num);
+				}
+				if (sign == '/'){
+					stack.push(stack.pop() / num);
+				}
+				sign = cs[i];
+				num = 0;
 			}
 		}
-		
-		Deque<Integer> numStack = new ArrayDeque<>();
-		Deque<Character> operatorStack = new ArrayDeque<>();
-		numStack.push(nums[0]);
-		for (int i = 0; i < operators.length; i++){
-			if (operators[i] == '*' || operators[i] == '/'){
-				numStack.push(nums[i+1]);
-				int a1 = numStack.pollFirst();
-				int a2 = numStack.pollFirst();
-				switch (operators[i]) {
-				case '*':{
-					numStack.push(a2 * a1);
-				}
-					break;
-				case '/':{
-					numStack.push(a2 / a1);
-				}
-					break;
-				default:
-					break;
-				}
-			}else{
-				operatorStack.push(operators[i]);
-				numStack.push(nums[i+1]);
-			}
+		int res = 0;
+		for (int i : stack){
+			res += i;
 		}
-		
-		while (!operatorStack.isEmpty()){
-			char opera = operatorStack.pollLast();
-			int a1 = numStack.pollLast();
-			int a2 = numStack.pollLast();
-			switch (opera) {
-			case '+':{
-				numStack.addLast(a1 + a2);;
-			}
-				break;
-			case '-':{
-				numStack.addLast(a1 - a2);
-			}
-				break;
-			default:
-				break;
-			}
-		}
-		
-		return numStack.peek();
-    }
+		return res;
+	}
 	
 	public static void main(String[] args) {
 		SolutionDay27_L0227 day = new SolutionDay27_L0227();
