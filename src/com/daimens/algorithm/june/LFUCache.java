4ffb75e3bcfafa23917de0cf228f6bf0642e54ab
demon song <787423495@qq.com>
2017-06-06 20:11:11 +0800

    460.LFU Cache

--- src/com/daimens/algorithm/june/LFUCache.java
+++ src/com/daimens/algorithm/june/LFUCache.java
@@ -1,5 +1,9 @@
 package com.daimens.algorithm.june;
 
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.Map;
+
 /**
  * 
  * @author DemonSong
@@ -21,16 +25,111 @@ package com.daimens.algorithm.june;
  *         
  **/
 public class LFUCache {
-
+	
+	class Node{
+		public int count;
+		public LinkedHashSet<Integer> keys;
+		public Node next;
+		public Node prev;
+		
+		public Node(int count){
+			this.count = count;
+			keys = new LinkedHashSet<>();
+			prev = next = null;
+		}
+	}
+	
+	private Node head = null;
+	private int cap = 0;
+	
+	Map<Integer, Integer> cache;
+	Map<Integer, Node> nodeHash;
     public LFUCache(int capacity) {
-        
+        cache = new HashMap<>();
+        nodeHash = new HashMap<>();
+        this.cap = capacity;
     }
     
-    public int get(int key) {
-        return 0;
+    private void increaseCount(int key){
+    	Node node = nodeHash.get(key);
+    	node.keys.remove(key);
+    	
+    	if (node.next == null){
+    		node.next = new Node(node.count + 1);
+    		node.next.prev = node;
+    		node.next.keys.add(key);
+    	}
+    	else if (node.next.count == node.count + 1){
+    		node.next.keys.add(key);
+    	}
+    	else{
+    		Node tmp = new Node(node.count + 1);
+    		tmp.keys.add(key);
+    		tmp.prev = node;
+    		tmp.next = node.next;
+    		node.next.prev = tmp;
+    		node.next = tmp;
+    	}
+    	
+    	nodeHash.put(key, node.next);
+    	if (node.keys.isEmpty()) remove(node);
     }
     
-    public void put(int key, int value) {
-        
+    private void addToHead(int key) {
+    	if (head == null){
+    		head = new Node(0);
+    		head.keys.add(key);
+    	}else if (head.count > 0){
+    		Node node = new Node(0);
+    		node.keys.add(key);
+    		node.next = head;
+    		head.prev = node;
+    		head = node;
+    	}else{
+    		head.keys.add(key);
+    	}
+    	nodeHash.put(key, head);
+    }
+    
+    private void removeOld(){
+    	if (head == null) return;
+    	int old = head.keys.iterator().next();
+    	head.keys.remove(old);
+    	if (head.keys.size() == 0) remove(head);
+    	nodeHash.remove(old);
+    	cache.remove(old);
+    }
+    
+    private void remove(Node node) {
+    	if (node.prev == null){
+    		head = node.next;
+    	}else{
+    		node.prev.next = node.next;
+    	}
+    	if (node.next != null){
+    		node.next.prev = node.prev;
+    	}
+    }
+    
+    public int get(int key) { //update
+    	if (!cache.containsKey(key)) return -1;
+    	increaseCount(key);
+    	return cache.get(key);
+    }
+    
+    public void put(int key, int value) { //remove
+    	if (cap == 0) return;
+    	if (cache.containsKey(key)){
+    		cache.put(key, value);
+    	}else{
+    		if (cache.size() < cap){
+    			cache.put(key, value);
+    		}else{
+    			removeOld();
+    			cache.put(key, value);
+    		}
+    		addToHead(key);
+    	}
+    	increaseCount(key);
     }
 }
