eba381f1d9e653471df602addced0c47d4879040
demon song <787423495@qq.com>
2017-06-07 15:28:17 +0800

    138.Copy List with Random Pointer

--- src/com/daimens/algorithm/june/SolutionDay07_L0138.java
+++ src/com/daimens/algorithm/june/SolutionDay07_L0138.java
@@ -14,5 +14,101 @@ package com.daimens.algorithm.june;
  *
  */
 public class SolutionDay07_L0138 {
+	
+//	public RandomListNode copyRandomList(RandomListNode head) {
+//		RandomListNode dummy = new RandomListNode(0);
+//		RandomListNode cur = dummy;
+//		
+//		RandomListNode pos = head;
+//		Map<Integer, RandomListNode> index = new HashMap<>();
+//		Map<RandomListNode, Integer> originIndex = new HashMap<>();
+//		
+//		int cnt = 0;
+//		while (pos != null){
+//			cur.next = new RandomListNode(pos.label);
+//			originIndex.put(pos, cnt);
+//			index.put(cnt++, cur.next);
+//			cur = cur.next;
+//			pos = pos.next;
+//		}
+//		
+//		pos = head;
+//		cur = dummy.next;
+//		
+//		while (pos != null){
+//			if (pos.random != null){
+//				int idx = originIndex.get(pos.random);
+//				cur.random = index.get(idx);
+//			}
+//			cur = cur.next;
+//			pos = pos.next;
+//		}
+//		return dummy.next;
+//    }
+	
+//	public RandomListNode copyRandomList(RandomListNode head) {
+//		if (head == null) return null;
+//		
+//		Map<RandomListNode, RandomListNode> map = new HashMap<>();
+//		RandomListNode pos = head;
+//		while (pos != null){
+//			map.put(pos, new RandomListNode(pos.label));
+//			pos = pos.next;
+//		}
+//		
+//		pos = head;
+//		while (pos != null){
+//			map.get(pos).next = map.get(pos.next);
+//			map.get(pos).random = map.get(pos.random);
+//			pos = pos.next;
+//		}
+//		return map.get(head);
+//	}
+	
+	public RandomListNode copyRandomList(RandomListNode head) {
+		if (head == null) return null;
+		RandomListNode pos = head;
+		while (pos != null){
+			RandomListNode copy = new RandomListNode(pos.label);
+			copy.next = pos.next;
+			pos.next = copy;
+			pos = pos.next.next;
+		}
+		
+		pos = head;
+		while (pos != null){
+			pos.next.random = pos.random != null ? pos.random.next : null;
+			pos = pos.next.next;
+		}
 
+		pos = head;
+		RandomListNode copy = pos.next;
+		RandomListNode curr = copy;
+		while (pos != null){
+			pos.next = pos.next.next;
+			pos = pos.next;
+			curr.next = pos != null ? pos.next : null;
+			curr = curr.next;
+		}
+		return copy;
+	}
+	
+	public static void main(String[] args) {
+		SolutionDay07_L0138 day = new SolutionDay07_L0138();
+		RandomListNode head = new RandomListNode(1);
+		head.next = new RandomListNode(2);
+		day.copyRandomList(head);
+	}
+}
+
+class RandomListNode{
+	int label;
+	RandomListNode next, random;
+	public RandomListNode(int x) {
+		this.label = x;
+	}
+	@Override
+	public String toString() {
+		return "[" + label + "]";
+	}
 }
