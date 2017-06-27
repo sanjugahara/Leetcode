dd7585330e9499c0340e1bf981bc41fd10e2de31
demon song <787423495@qq.com>
2017-06-27 16:15:39 +0800

    2100.Graveyard Design

---
+++ src/com/daimens/algorithm/june/SolutionDay27_P2100.java
@@ -0,0 +1,191 @@
+package com.daimens.algorithm.june;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.List;
+
+public class SolutionDay27_P2100 {
+	InputStream is;
+	PrintWriter out;
+	String INPUT = "./data/judge/2100.txt";
+	
+	class Pair{
+		long l;
+		long r;
+		public Pair(long l, long r){
+			this.l = l;
+			this.r = r;
+		}
+	}
+	
+	void solve() {
+		long num = nl();
+		long n = (int)Math.sqrt(num);
+		long lb = 1, rb = 1;
+		long sum = 0;
+		List<Pair> list = new ArrayList<Pair>();
+		for(;;){
+			while (lb <= n && sum < num){
+				sum += (rb * rb);
+				rb++;
+			}
+			if (sum < num) break;
+			if (sum == num){
+				list.add(new Pair(lb,rb - 1));
+			}
+			sum -= lb * lb;
+			lb++;
+		}
+		out.println(list.size());
+		for (Pair p : list){
+			long size = p.r - p.l + 1;
+			StringBuilder sb = new StringBuilder();
+			sb.append(size + " ");
+			for (long i = p.l; i <= p.r; ++i){
+				sb.append(i + " ");
+			}
+			out.println(sb.toString().trim());
+		}
+	}
+	
+	void run() throws Exception {
+		is = oj ? System.in : new FileInputStream(new File(INPUT));
+		out = new PrintWriter(System.out);
+
+		long s = System.currentTimeMillis();
+		solve();
+		out.flush();
+		tr(System.currentTimeMillis() - s + "ms");
+	}
+
+	public static void main(String[] args) throws Exception {
+		new SolutionDay27_P2100().run();
+	}
+
+	private byte[] inbuf = new byte[1024];
+	public int lenbuf = 0, ptrbuf = 0;
+
+	private int readByte() {
+		if (lenbuf == -1)
+			throw new InputMismatchException();
+		if (ptrbuf >= lenbuf) {
+			ptrbuf = 0;
+			try {
+				lenbuf = is.read(inbuf);
+			} catch (IOException e) {
+				throw new InputMismatchException();
+			}
+			if (lenbuf <= 0)
+				return -1;
+		}
+		return inbuf[ptrbuf++];
+	}
+
+	private boolean isSpaceChar(int c) {
+		return !(c >= 33 && c <= 126);
+	}
+
+	private int skip() {
+		int b;
+		while ((b = readByte()) != -1 && isSpaceChar(b))
+			;
+		return b;
+	}
+
+	private double nd() {
+		return Double.parseDouble(ns());
+	}
+
+	private char nc() {
+		return (char) skip();
+	}
+
+	private String ns() {
+		int b = skip();
+		StringBuilder sb = new StringBuilder();
+		while (!(isSpaceChar(b))) { // when nextLine, (isSpaceChar(b) && b != '
+									// ')
+			sb.appendCodePoint(b);
+			b = readByte();
+		}
+		return sb.toString();
+	}
+
+	private char[] ns(int n) {
+		char[] buf = new char[n];
+		int b = skip(), p = 0;
+		while (p < n && !(isSpaceChar(b))) {
+			buf[p++] = (char) b;
+			b = readByte();
+		}
+		return n == p ? buf : Arrays.copyOf(buf, p);
+	}
+
+	private char[][] nm(int n, int m) {
+		char[][] map = new char[n][];
+		for (int i = 0; i < n; i++)
+			map[i] = ns(m);
+		return map;
+	}
+
+	private int[] na(int n) {
+		int[] a = new int[n];
+		for (int i = 0; i < n; i++)
+			a[i] = ni();
+		return a;
+	}
+
+	private int ni() {
+		int num = 0, b;
+		boolean minus = false;
+		while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'))
+			;
+		if (b == '-') {
+			minus = true;
+			b = readByte();
+		}
+
+		while (true) {
+			if (b >= '0' && b <= '9') {
+				num = num * 10 + (b - '0');
+			} else {
+				return minus ? -num : num;
+			}
+			b = readByte();
+		}
+	}
+
+	private long nl() {
+		long num = 0;
+		int b;
+		boolean minus = false;
+		while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'))
+			;
+		if (b == '-') {
+			minus = true;
+			b = readByte();
+		}
+
+		while (true) {
+			if (b >= '0' && b <= '9') {
+				num = num * 10 + (b - '0');
+			} else {
+				return minus ? -num : num;
+			}
+			b = readByte();
+		}
+	}
+
+	private boolean oj = System.getProperty("ONLINE_JUDGE") != null;
+
+	private void tr(Object... o) {
+		if (!oj)
+			System.out.println(Arrays.deepToString(o));
+	}
+}
