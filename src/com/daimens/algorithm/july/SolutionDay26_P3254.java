5f29e6073e0f1cddc3eae0ca8ebe7096a5a022d3
demon song <787423495@qq.com>
2017-07-26 16:20:39 +0800

    3254.Corn fields

---
+++ src/com/daimens/algorithm/july/SolutionDay26_P3254.java
@@ -0,0 +1,204 @@
+package com.daimens.algorithm.july;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.InputMismatchException;
+
+public class SolutionDay26_P3254 {
+	InputStream is;
+	PrintWriter out;
+	String INPUT = "./data/judge/201707/3254.txt";
+	
+	static final int MOD = 1000000000; 
+	int N = 0;
+	int M = 0;
+	void solve() {
+		N = ni();
+		M = ni();
+		boolean[][] board = new boolean[N][M];
+		for (int i = 0; i < N; ++i){
+			for (int j = 0; j < M; ++j){
+				if (ni() == 1) board[i][j] = true;
+			}
+		}
+		
+		int[][] dp = new int[N][1 << M];
+		for (int i = 0; i < 1 << M; ++i){
+			if (valid(i, board[0])){
+				dp[0][i] = 1;
+			}
+		}
+		
+		for (int i = 1; i < N; ++i){
+			for (int j = 0; j < 1 << M; ++j){
+				if (valid(j, board[i])){
+					for (int s = 0; s < 1 << M; ++s){
+						if ((j & s) == 0){
+							dp[i][j] = (dp[i][j] + dp[i - 1][s]) % MOD;
+						}
+					}
+				}
+			}
+		}
+		
+		int sum = 0;
+		for (int i = 0; i < 1 << M; ++i){
+			if (valid(i, board[N - 1])){
+				sum = (sum + dp[N - 1][i]) % MOD;
+			}
+		}
+		out.println(sum);
+	}
+	
+	public boolean valid(int s, boolean[] board){
+		for (int i = 0; i < M; ++i){
+			if ((s & (1 << i)) != 0){
+				if (!board[i]) return false;
+				if ((i + 1 < M) && (s & (1 << (i + 1))) != 0) return false;
+				if ((i - 1 >= 0) && (s & (1 << (i - 1))) != 0) return false;
+			}
+		}
+		return true;
+	}
+	
+	void run() throws Exception {
+		is = oj ? System.in : new FileInputStream(new File(INPUT));
+		out = new PrintWriter(System.out);
+
+		long s = System.currentTimeMillis();
+		solve();
+		out.flush();
+		tr(System.currentTimeMillis() - s + "ms");
+	}
+
+	public static void main(String[] args) throws Exception {
+		new SolutionDay26_P3254().run();
+	}
+
+	private byte[] inbuf = new byte[1024];
+	public int lenbuf = 0, ptrbuf = 0;
+
+	private int readByte() {
+		if (lenbuf == -1)
+			throw new InputMismatchException();
+		if (ptrbuf >= lenbuf) {
+			ptrbuf = 0;
+			try {
+				lenbuf = is.read(inbuf);
+			} catch (IOException e) {
+				throw new InputMismatchException();
+			}
+			if (lenbuf <= 0)
+				return -1;
+		}
+		return inbuf[ptrbuf++];
+	}
+
+	private boolean isSpaceChar(int c) {
+		return !(c >= 33 && c <= 126);
+	}
+
+	private int skip() {
+		int b;
+		while ((b = readByte()) != -1 && isSpaceChar(b))
+			;
+		return b;
+	}
+
+	private double nd() {
+		return Double.parseDouble(ns());
+	}
+
+	private char nc() {
+		return (char) skip();
+	}
+
+	private String ns() {
+		int b = skip();
+		StringBuilder sb = new StringBuilder();
+		while (!(isSpaceChar(b))) { // when nextLine, (isSpaceChar(b) && b != '
+									// ')
+			sb.appendCodePoint(b);
+			b = readByte();
+		}
+		return sb.toString();
+	}
+
+	private char[] ns(int n) {
+		char[] buf = new char[n];
+		int b = skip(), p = 0;
+		while (p < n && !(isSpaceChar(b))) {
+			buf[p++] = (char) b;
+			b = readByte();
+		}
+		return n == p ? buf : Arrays.copyOf(buf, p);
+	}
+
+	private char[][] nm(int n, int m) {
+		char[][] map = new char[n][];
+		for (int i = 0; i < n; i++)
+			map[i] = ns(m);
+		return map;
+	}
+
+	private int[] na(int n) {
+		int[] a = new int[n];
+		for (int i = 0; i < n; i++)
+			a[i] = ni();
+		return a;
+	}
+
+	private int ni() {
+		int num = 0, b;
+		boolean minus = false;
+		while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'))
+			;
+		if (b == '-') {
+			minus = true;
+			b = readByte();
+		}
+
+		while (true) {
+			if (b >= '0' && b <= '9') {
+				num = num * 10 + (b - '0');
+			} else {
+				return minus ? -num : num;
+			}
+			b = readByte();
+		}
+	}
+
+	private long nl() {
+		long num = 0;
+		int b;
+		boolean minus = false;
+		while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'))
+			;
+		if (b == '-') {
+			minus = true;
+			b = readByte();
+		}
+
+		while (true) {
+			if (b >= '0' && b <= '9') {
+				num = num * 10 + (b - '0');
+			} else {
+				return minus ? -num : num;
+			}
+			b = readByte();
+		}
+	}
+
+	private boolean oj = System.getProperty("ONLINE_JUDGE") != null;
+
+	private void tr(Object... o) {
+		if (!oj)
+			System.out.println(Arrays.deepToString(o));
+	}
+}
+
+
