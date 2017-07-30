d0e8fff2bbbcdb30598dc838a58b05e96acae160
demon song <787423495@qq.com>
2017-07-30 14:52:13 +0800

    651.4 Keys Keyboard

--- src/com/daimens/algorithm/july/SolutionDay30_L0503.java
+++ src/com/daimens/algorithm/july/SolutionDay30_L0503.java
@@ -1,9 +1,21 @@
 package com.daimens.algorithm.july;
 
 public class SolutionDay30_L0503 {
-	
+
+	public int maxA(int N) {
+		int[] dp = new int[N + 16];
+		for (int i = 0; i <= N; ++i) dp[i] = i;
+		for (int i = 1; i <= N; ++i){
+			for (int j = i + 3; j <= N; ++j){
+				dp[j] = Math.max(dp[j], (j - i - 1) * dp[i]);
+			}
+		}
+		return dp[N];
+	}
+
 	public static void main(String[] args) {
 		SolutionDay30_L0503 day = new SolutionDay30_L0503();
+		System.out.println(day.maxA(3));
 	}
 
 }
--- src/com/daimens/algorithm/july/SolutionDay30_L0650.java
+++ src/com/daimens/algorithm/july/SolutionDay30_L0650.java
@@ -4,14 +4,27 @@ import java.util.Arrays;
 
 public class SolutionDay30_L0650 {
 	
+//	final int INF = 1 << 29;
+//	public int minSteps(int n) {
+//		int[] dp = new int[n + 16];
+//		Arrays.fill(dp, INF);
+//		dp[1] = 0;
+//		for (int i = 1; i < n; ++i){
+//			for (int j = 2 * i; j <= n; j += i){
+//				dp[j] = Math.min(dp[j], dp[i] + j / i);
+//			}
+//		}
+//		return dp[n];
+//	}
+	
 	final int INF = 1 << 29;
 	public int minSteps(int n) {
 		int[] dp = new int[n + 16];
 		Arrays.fill(dp, INF);
 		dp[1] = 0;
 		for (int i = 1; i < n; ++i){
-			for (int j = 2 * i; j <= n; j += i){
-				dp[j] = Math.min(dp[j], dp[i] + j / i);
+			for (int j = 1; (j + 1) * i <= n; ++j){
+				dp[(j + 1) * i] = Math.min(dp[(j + 1) * i], dp[i] + j + 1);
 			}
 		}
 		return dp[n];
