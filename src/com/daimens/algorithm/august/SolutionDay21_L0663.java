18ed3d6026865d5ae5d650667f3d258b33d78907
demon song <787423495@qq.com>
2017-08-21 19:03:06 +0800

    663.Equal Tree Partition

--- src/com/daimens/algorithm/august/SolutionDay21_L0663.java
+++ src/com/daimens/algorithm/august/SolutionDay21_L0663.java
@@ -3,27 +3,15 @@ package com.daimens.algorithm.august;
 import java.util.HashSet;
 import java.util.Set;
 
-public class SolutionDay21_L0502 {
+public class SolutionDay21_L0663 {
 	
 	public boolean checkEqualTree(TreeNode root) {
-		if (root == null) return false;
-		dfs(root);
+		if (root.left == null && root.right == null) return false;
+		list = new HashSet<>();
+        int sum = find(root);
 		if (sum % 2 != 0) return false;
 		int key = sum / 2;
-		list = new HashSet<>();
-		int x = find(root);
-		if (x == 0){
-			if (root.left == null && root.right == null) return false;
-		}
 		return list.contains(key);
-	}
-	
-	int sum = 0;
-	public void dfs(TreeNode root){
-		if (root == null) return;
-		sum += root.val;
-		dfs(root.left);
-		dfs(root.right);
 	}
 	
 	Set<Integer> list;
@@ -36,7 +24,7 @@ public class SolutionDay21_L0502 {
 	}
 	
 	public static void main(String[] args) {
-		SolutionDay21_L0502 day = new SolutionDay21_L0502();
+		SolutionDay21_L0663 day = new SolutionDay21_L0663();
 		TreeNode root = new TreeNode(5);
 		root.left = new TreeNode(10);
 		root.right = new TreeNode(10);
